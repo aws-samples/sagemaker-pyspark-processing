b18af4f3b69183bb6d7ab7d5b20ee08b0320ba94
Maira Ladeira Tanke <mttanke@amazon.com>
2022-09-01 18:38:09 -0700

    Adding basic infrastructure example and sample data

--- ml_pipeline/run_pipeline_locally/run_pipeline.sh
+++ ml_pipeline/run_pipeline_locally/run_pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e # exit script if any command fails
 
+# TODO: Populate those variables with the Infra and Data buckets names
+INFRA_S3_BUCKET="<INFRA_S3_BUCKET>"
+DATA_S3_BUCKET="<DATA_S3_BUCKET>"
+
 # installing sagemaker to run code locally 
 pip install -r ../../requirements.txt
 pip install -U sagemaker
@@ -18,9 +22,12 @@ cd $SCRIPTDIR
 cd ../../
 # sync src folder 
 # saving src versioned for auditability
-aws s3 sync src s3://<INFRA_S3_BUCKET>/src
+aws s3 sync src s3://$INFRA_S3_BUCKET/src
 # sync ml_pipeline folder
-aws s3 sync ml_pipeline s3://<INFRA_S3_BUCKET>/ml_pipeline
+aws s3 sync ml_pipeline s3://$INFRA_S3_BUCKET/ml_pipeline
+
+# copy sample data to S3
+aws s3 cp sample_data/abalone_data.csv s3://$DATA_S3_BUCKET/data_input/abalone_data.csv
 
 # run 
 python ml_pipeline/pipeline.py
